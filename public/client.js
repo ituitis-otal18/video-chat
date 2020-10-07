2dc312b2019a9bd8c6352fc2ae0077985a497a41
otal18 <hakan.otal@outlook.com>
2020-10-07 17:46:17 +0300

    first commit

---
+++ public/client.js
@@ -0,0 +1,108 @@
+//Socket
+const socket = io('/');
+const peer = new Peer(undefined, {
+    host: 'localhost',
+    port: 9000,
+    path: '/peer'
+});
+
+const peers = {}
+let allUsers = [];
+let otherUsers = [];
+
+//Create own Video
+const videoGrid = document.getElementById("video-grid");
+const myVideo = document.getElementById("myVideo");
+myVideo.muted = true;
+
+navigator.getUserMedia(
+    { video: true, audio: true },
+    stream => { 
+        addVideoStream(myVideo, stream);
+
+        peer.on('call', call => {
+            call.answer(stream)
+
+            //Send video stream
+            const video = document.createElement('video')
+            call.on('stream', userVideoStream => {
+                addVideoStream(video, userVideoStream)
+            })
+            call.on('close', () => {
+                video.remove();
+            })
+        })
+
+        socket.on('user-joined', (userID) => {
+            otherUsers.push(userID);
+            connectToUser(userID, stream);
+        })
+    },
+    error => { console.warn(error.message) }
+);
+
+//Socket
+socket.on('connect', () => {
+    peer.on('open', id => {
+        socket.emit('join', roomID, id);
+    })
+});
+
+socket.on('all-users', users => {
+    allUsers = users;
+})
+
+socket.on('user-disconnected', userID => {
+    if (peers[userID]) peers[userID].close();
+})
+
+function connectToUser(userID, stream){
+    const call = peer.call(userID, stream);
+
+    //Get video stream
+    const video = document.createElement("video");
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream);
+    })
+    call.on('close', () => {
+        video.remove();
+    })
+    peers[userID] = call
+}
+
+function addVideoStream(video, stream){
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () => {
+        video.play();
+    })
+    videoGrid.append(video);
+}
+
+//Chat
+function sendMessage(){
+    let msg = userName + ": " + document.getElementById("message").value;
+    socket.emit('new-message', roomID, msg);
+}
+
+socket.on('chat-update', chat => {
+    let area = document.getElementById("chat");
+    area.innerHTML = "";
+    let br = document.createElement("br");
+
+    chat.forEach(msg => {
+        let p = document.createElement("p");
+        p.innerHTML = msg;
+        area.append(p);
+        area.append(br);
+    });
+    console.log(chat);
+})
+
+function copyURL(){
+    var chatLink = document.getElementById("chat-link");
+    chatLink.value = window.location.origin + "/room/" + roomID;
+
+    chatLink.select();
+    chatLink.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+}
---
+++ server.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const { v4: uuidV4 } = require('uuid')
+
+const { PeerServer } = require('peer');
+const peerServer = PeerServer({ port: 9000, path: '/peer' });
+
+//APP
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}))
+
+//home
+app.get('/', (req, res) => {
+    res.render('index', { 
+        id: ""
+    });
+})
+
+//coming with link
+app.get('/room/:id', (req, res) => {
+    res.render('index', { 
+        id: req.params.id
+    });
+})
+
+//create room or join
+app.post('/room', (req, res) => {
+    if(req.body.roomId) res.redirect(`/room/${req.body.roomId}/${req.body.userName}`);
+    else res.redirect(`/room/${uuidV4()}/${req.body.userName}`);
+})
+
+//connect to room
+app.get('/room/:id/:name', (req, res) => {
+    res.render('room', { 
+        id: req.params.id,
+        name: req.params.name
+    });
+})
+
+server.listen(port);
+
+
+let users = [];
+let chat = {
+    id: "",
+    log: []
+};
+
+//Socket
+io.on('connection', socket => {
+
+    socket.on('join', (roomID, userID) => {
+        chat.id = roomID;
+
+        users.push(userID);
+        socket.join(roomID);
+       
+        io.in(roomID).emit('all-users', users);
+        io.in(roomID).emit('chat-update', chat.log);
+
+        socket.to(roomID).broadcast.emit('user-joined', userID);
+        console.log(userID+" joined room: "+roomID);
+        
+        socket.on('disconnect', () => {
+            users.splice(users.indexOf(userID), 1);
+            socket.to(roomID).broadcast.emit('user-disconnected', userID)
+            console.log(userID+" left room: "+roomID);
+            
+            if(users.length == 0)chat.id = "";
+        })
+
+        socket.on('new-message', (id, msg) => {
+            if(id == chat.id) chat.log.push(msg);
+            io.in(roomID).emit('chat-update', chat.log);
+        })
+    })
+
+})
